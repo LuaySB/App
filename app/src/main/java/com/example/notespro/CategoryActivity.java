f4da96ec8280dc303a9bbbc7065d0e8c007592eb
luaya <luai.alsabbagh.98@gmail.com>
2022-10-15 19:50:16 +0200

    La till flera kategorier i CategoryActivity.

---
+++ app/src/main/java/com/example/notespro/CategoryActivity.java
@@ -0,0 +1,175 @@
+package com.example.notespro;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ArrayList<Products> productsArrayList;
+    MyAdapter myAdapter;
+    FirebaseFirestore db;
+    ProgressDialog progressDialog;
+    String kategori;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Fetching Data...");
+        progressDialog.show();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        kategori = getIntent().getStringExtra("kategori");
+
+        db = FirebaseFirestore.getInstance();
+        productsArrayList = new ArrayList<Products>();
+        myAdapter = new MyAdapter(CategoryActivity.this, productsArrayList);
+
+        recyclerView.setAdapter(myAdapter);
+
+        EventChangeListener();
+    }
+
+    private void EventChangeListener() {
+
+        Log.d("MyTag", "Entering store: " + kategori);
+
+        db.collection("ica").orderBy("kategori").startAt(kategori).endAt(kategori + "\uf8ff")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+
+                        if (error != null){
+
+                            if(progressDialog.isShowing())
+                                progressDialog.dismiss();
+
+                            Log.e("Firestore error!",error.getMessage());
+                            return;
+                        }
+
+                        for (DocumentChange dc: value.getDocumentChanges()){
+                            if (dc.getType() == DocumentChange.Type.ADDED){
+                                Products product = dc.getDocument().toObject(Products.class);
+                                product.setPrice(product.getPrice().replaceAll("[^.0123456789]",""));
+                                productsArrayList.add(product);
+                            }
+
+                            myAdapter.notifyDataSetChanged();
+                            if(progressDialog.isShowing())
+                                progressDialog.dismiss();
+
+                        }
+                    }
+                });
+
+        db.collection("lidl").orderBy("kategori").startAt(kategori).endAt(kategori + "\uf8ff")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+
+                        if (error != null){
+
+                            if(progressDialog.isShowing())
+                                progressDialog.dismiss();
+
+                            Log.e("Firestore error!",error.getMessage());
+                            return;
+                        }
+
+                        for (DocumentChange dc: value.getDocumentChanges()){
+                            if (dc.getType() == DocumentChange.Type.ADDED){
+                                Products product = dc.getDocument().toObject(Products.class);
+                                product.setPrice(product.getPrice().replaceAll("[^.0123456789]",""));
+                                productsArrayList.add(product);
+                            }
+
+                            myAdapter.notifyDataSetChanged();
+                            if(progressDialog.isShowing())
+                                progressDialog.dismiss();
+
+                        }
+                    }
+                });
+
+        db.collection("coop välsviken").orderBy("kategori").startAt(kategori).endAt(kategori + "\uf8ff")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+
+                        if (error != null){
+
+                            if(progressDialog.isShowing())
+                                progressDialog.dismiss();
+
+                            Log.e("Firestore error!",error.getMessage());
+                            return;
+                        }
+
+                        for (DocumentChange dc: value.getDocumentChanges()){
+                            if (dc.getType() == DocumentChange.Type.ADDED){
+                                Products product = dc.getDocument().toObject(Products.class);
+                                product.setPrice(product.getPrice().replaceAll("[^.0123456789]",""));
+                                productsArrayList.add(product);
+                            }
+
+                            myAdapter.notifyDataSetChanged();
+                            if(progressDialog.isShowing())
+                                progressDialog.dismiss();
+
+                        }
+                    }
+                });
+
+        db.collection("coop kronoparken").orderBy("kategori").startAt(kategori).endAt(kategori + "\uf8ff")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+
+                        if (error != null){
+
+                            if(progressDialog.isShowing())
+                                progressDialog.dismiss();
+
+                            Log.e("Firestore error!",error.getMessage());
+                            return;
+                        }
+
+                        for (DocumentChange dc: value.getDocumentChanges()){
+                            if (dc.getType() == DocumentChange.Type.ADDED){
+                                Products product = dc.getDocument().toObject(Products.class);
+                                product.setPrice(product.getPrice().replaceAll("[^.0123456789]",""));
+                                productsArrayList.add(product);
+                            }
+
+                            myAdapter.notifyDataSetChanged();
+                            if(progressDialog.isShowing())
+                                progressDialog.dismiss();
+
+                        }
+                    }
+                });
+    }
+}
